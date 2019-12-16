1f035abe1aba886c366cfa985b49c7e7df0d9215
Egor <likeroge@gmail.com>
2019-12-16 22:56:04 +0400

    initial commi

---
+++ indecision-app/src/app.js
@@ -0,0 +1,52 @@
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        )
+    }
+}
+
+class Action extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component{
+    render(){
+        return(
+            <div>
+                Options component here
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component{
+    render(){
+        return(
+            <div>
+                Add Option Component here
+            </div>
+        )
+    }
+}
+
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header/>
+        <Action/>
+        <Options/>
+        <AddOption/>
+    </div>
+)
+
+ReactDOM.render(jsx,document.getElementById('app'))
