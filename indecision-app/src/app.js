02b29e5cbfa823569cba0e09a3c4c9528740675b
Egor <likeroge@gmail.com>
2019-12-16 23:31:20 +0400

    secont commit

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component{
+    render(){
+        return (
+            <div>
+                <Header/>
+                <Action/>
+                <Options/>
+                <AddOption/>
+            </div>
+        )
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -23,7 +36,18 @@ class Options extends React.Component{
     render(){
         return(
             <div>
-                Options component here
+                Option component here
+                <Option/>
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component{
+    render(){
+        return(
+            <div>
+                Option component here
             </div>
         )
     }
@@ -33,20 +57,12 @@ class AddOption extends React.Component{
     render(){
         return(
             <div>
-                Add Option Component here
+                Add Option Component here 
             </div>
         )
     }
 }
 
-const jsx = (
-    <div>
-        <h1>Title</h1>
-        <Header/>
-        <Action/>
-        <Options/>
-        <AddOption/>
-    </div>
-)
 
-ReactDOM.render(jsx,document.getElementById('app'))
+
+ReactDOM.render(<IndecisionApp/>,document.getElementById('app'))
